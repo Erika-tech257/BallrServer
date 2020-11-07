42c2e09a2f49887f9689a8e9e9e54fdeecf2c5ac
Nolan Shirley <nolanbshirley@gmail.com>
2020-11-07 18:27:26 -0500

    added .env file and structure for app.js

---
+++ App.js
@@ -0,0 +1,21 @@
+require('dotenv').config(); 
+
+const Express = require('express'); 
+
+const app = Express(); 
+
+const database = require('./Db'); 
+
+database.sync(); 
+
+app.use(Express.json()); 
+
+app.use(require('./middleware/headers')); 
+
+app.use(Express.static(_dirname + '/public')); 
+
+app.get('/', (req, res) => res.render('index')); 
+
+
+
+app.listen(process.env.PORT, function(){console.log(`app is listening on port ${process.env.PORT}`)})
